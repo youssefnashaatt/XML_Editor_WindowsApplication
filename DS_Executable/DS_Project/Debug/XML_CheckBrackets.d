42f7a939a512f61a940034c7a5282adbd4f171a7
youssefnashaatt <nashaatyoussef01@gmail.com>
2023-01-30 00:52:19 +0200

    Add executable

---
+++ DS_Executable/DS_Project/Debug/XML_CheckBrackets.d
@@ -0,0 +1,2 @@
+XML_CheckBrackets.o: ../XML_CheckBrackets.cpp ../XML_CheckBrackets.h
+../XML_CheckBrackets.h:
---
+++ DS_Executable/DS_Project/Debug/XML_Minifying.d
@@ -0,0 +1,2 @@
+XML_Minifying.o: ../XML_Minifying.cpp ../XML_Minifying.h
+../XML_Minifying.h:
